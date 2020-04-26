7aa72d180cbcaae0ca88dab7768cbe1c6d8b1bfd
Pau Matas <paumatasalbi@gmail.com>
2020-04-26 18:05:47 +0200

    paquets necessaris i funcions requerides del guide

--- guide.py
+++ guide.py
@@ -1,15 +1,24 @@
 import networkx as nx
 import osmnx as ox
+from haversine import haversine
+from staticmap import StaticMap, Line, IconMarker
 
 
 download_graph(place):
+    """ ... """
 
 save_graph(graph, filename):
+    """ ... """
 
 load_graph(filename):
+    """ ... """
 
 print_graph(graph):
+    """ ... """
 
 get_directions(graph, source_location, destination_location):
+    """ ... """
 
-plot_directions(graph, source_location, destination_location, directions, filename, width=400, height=400):
+plot_directions(graph, source_location, destination_location, directions,
+                filename, width=400, height=400):
+    """ ... """
