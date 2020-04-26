817227604d7d10baf7333a4f7c3409b2268135d5
Pau Matas <paumatasalbi@gmail.com>
2020-04-26 16:30:36 +0200

    paquets i funcions requerides al guide

---
+++ guide.py
@@ -0,0 +1,15 @@
+import networkx as nx
+import osmnx as ox
+
+
+download_graph(place):
+
+save_graph(graph, filename):
+
+load_graph(filename):
+
+print_graph(graph):
+
+get_directions(graph, source_location, destination_location):
+
+plot_directions(graph, source_location, destination_location, directions, filename, width=400, height=400):
