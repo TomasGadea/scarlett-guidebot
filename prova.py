b548a161519bce933aca39173f00debf5cac7733
Tomás Gadea <01tomas.gadea@gmail.com>
2020-04-27 22:57:05 +0200

    afegit prova.py

--- prova.py
+++ prova.py
@@ -1,14 +1,2 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-import jutge
-
-G = nx.petersen_graph()
-plt.subplot(121)
-
-nx.draw(G, with_labels=True, font_weight='bold')
-plt.subplot(122)
-
-nx.draw_shell(G, nlist=[range(5, 10), range(5)],
-              with_labels=True, font_weight='bold')
-
-plt.show()
+import osmnx as ox
+ox.plot_graph(ox.graph_from_place('Modena, Italy', simplify=True))
