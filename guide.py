2c14a05cc0993d8e2e328144b556ec1c282f326d
Tomás Gadea <01tomas.gadea@gmail.com>
2020-04-28 14:56:41 +0200

    guide pep8

--- guide.py
+++ guide.py
@@ -6,26 +6,32 @@ import pickle
 import matplotlib.pyplot as plt
 
 
-download_graph(place):
+def download_graph(place):
     """ ... """
 
-save_graph(graph, filename):
+
+def save_graph(graph, filename):
     """ ... """
 
-load_graph(filename):
+
+def load_graph(filename):
     """ This function returns a graph read from a pickle file. """
     f = open('filename', 'rb')
     graph = pickle.load(f)
     f.close()
     return graph
 
-print_graph(graph):
+
+def print_graph(graph):
     """ This function draws graphicaly the graph graph. """
     nx.draw(graph, with_labels=True, font_weight='bold')
+    print(nx.info(graph))
 
-get_directions(graph, source_location, destination_location):
+
+def get_directions(graph, source_location, destination_location):
     """ ... """
 
-plot_directions(graph, source_location, destination_location, directions,
-                filename, width=400, height=400):
+
+def plot_directions(graph, source_location, destination_location, directions,
+                    filename, width=400, height=400):
     """ ... """
--- prova.py
+++ prova.py
@@ -1,2 +1,24 @@
 import osmnx as ox
-ox.plot_graph(ox.graph_from_place('Modena, Italy', simplify=True))
+import networkx as nx
+import matplotlib.pyplot as plt
+import pickle
+
+
+def download_graph(place):
+    graph = ox.graph_from_place(
+        place, network_type='drive', simplify=True)
+    ox.geo_utils.add_edge_bearings(graph)
+    return graph
+
+
+def save_graph(graph, filename):
+    f = open(filename, 'wb')
+    pickle.dump(graph, f)
+    f.close()
+
+
+def main():
+    mapa = download_graph("Berga, Spain")
+
+
+main()
